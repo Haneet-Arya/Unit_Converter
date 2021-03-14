e4c41d1af49ab04cbb672d72d7e41ee2f7bb2f65
Haneet-Arya <haneetarya16@gmail.com>
2021-03-14 12:31:43 +0530

    Initial Commit

---
+++ app/src/main/java/com/haneetarya/unitconverter/MainActivity.java
@@ -0,0 +1,35 @@
+package com.haneetarya.unitconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private TextView textView;
+    private EditText inputText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.Convertbutton);
+        textView = findViewById(R.id.outputText);
+        inputText = findViewById(R.id.KgValue);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Conversion Successful", Toast.LENGTH_SHORT).show();
+                String s = inputText.getText().toString();
+                int kg = Integer.parseInt(s);
+                double pound = 2.205*kg;
+                textView.setText("The Corresponding value in pound is " + pound);
+            }
+        });
+    }
+}
